1d242d643c639b28d35f81007d2bfacf8cedf346
Mike Dietrich <82385796+MikeDietrichDE@users.noreply.github.com>
2024-01-15 19:15:22 +0100

    Create check_patches_size.sql

---
+++ check_patches_size.sql
@@ -0,0 +1,40 @@
+-- -----------------------------------------------------------------------------------
+-- File Name    : https://github.com/MikeDietrichDE/scripts/blob/main/check_patches_size.sql
+-- Author       : Mike Dietrich
+-- Description  : Displays the space being used to store patch rollback zip files
+-- Requirements : Access to the DBA role.
+-- Call Syntax  : @check_patches_size.sql
+-- Last Modified: 15/01/2024
+-- Database Rel.: Oracle 19c
+-- -----------------------------------------------------------------------------------
+
+
+column patch_id format 9999999999
+column ru_version format a15
+column lob_size_md format 9999
+COLUMN ru_build_ts FORMAT A20
+
+
+set linesize 100
+set pagesize 300
+
+ALTER SESSION SET NLS_TIMESTAMP_FORMAT = 'MM/DD/YY HH24:MI';
+ALTER SESSION SET "_EXCLUDE_SEED_CDB_VIEW" = FALSE;
+
+SELECT
+    patch_id,
+    ru_version,
+    TO_CHAR(ru_build_timestamp, 'MM/DD/YYYY HH24:MI:SS') AS ru_build_ts,
+    round(dbms_lob.getlength(patch_directory) / 1024 / 1024) lob_size_mb
+FROM
+    sys.registry$sqlpatch_ru_info;
+
+
+SELECT
+   con_id, round(sum(dbms_lob.getlength(patch_directory) / 1024 / 1024)) total_lob_size_mba
+FROM
+   containers(sys.registry$sqlpatch_ru_info)
+GROUP BY
+   con_id
+ORDER BY
+   con_id;
